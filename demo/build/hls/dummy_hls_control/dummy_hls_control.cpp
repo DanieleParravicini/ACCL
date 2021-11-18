d64bc78f9a42a0bb06673187765dcfe82fc5d8a1
danielep <danielep@xilinx.com>
2021-11-18 17:52:37 +0000

    Adds a timer to hls_control

--- demo/build/hls/dummy_hls_control/dummy_hls_control.cpp
+++ demo/build/hls/dummy_hls_control/dummy_hls_control.cpp
@@ -13,64 +13,69 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-# *******************************************************************************/
+#
+*******************************************************************************/
 
-#include "hls_stream.h"
 #include "ap_int.h"
 #include "ap_utils.h"
+#include "hls_stream.h"
 
 using namespace hls;
 using namespace std;
 
-void dummy_hls_control(	ap_uint<32> scenario,
-				ap_uint<32> len,
-				ap_uint<32> comm,
-				ap_uint<32> root_src_dst,
-				ap_uint<32> function,
-				ap_uint<32> msg_tag,
-				ap_uint<32> buf0_type,
-				ap_uint<32> buf1_type,
-				ap_uint<32> buf2_type,
-				ap_uint<64> addra,
-				ap_uint<64> addrb,
-				ap_uint<64> addrc,
-				stream<ap_uint<512>> &cmd,
-				stream<ap_uint<32>>  &sts
-) {
-#pragma HLS INTERFACE s_axilite port=scenario
-#pragma HLS INTERFACE s_axilite port=len
-#pragma HLS INTERFACE s_axilite port=comm
-#pragma HLS INTERFACE s_axilite port=root_src_dst
-#pragma HLS INTERFACE s_axilite port=function
-#pragma HLS INTERFACE s_axilite port=msg_tag
-#pragma HLS INTERFACE s_axilite port=buf0_type
-#pragma HLS INTERFACE s_axilite port=buf1_type
-#pragma HLS INTERFACE s_axilite port=buf2_type
-#pragma HLS INTERFACE s_axilite port=addra
-#pragma HLS INTERFACE s_axilite port=addrb
-#pragma HLS INTERFACE s_axilite port=addrc
-#pragma HLS INTERFACE axis port=cmd
-#pragma HLS INTERFACE axis port=sts
-#pragma HLS INTERFACE s_axilite port=return
-	
-	ap_uint<512> in_data ;
-	//Input stream needs to be optimized in the same way as hostctrl 
-    in_data.range(31,0) 	= scenario 		;
-    in_data.range(63,32) 	= len 			;
-    in_data.range(95,64) 	= comm			;
-    in_data.range(127,96) 	= root_src_dst	;
-    in_data.range(159,128) 	= function 		;
-    in_data.range(191,160) 	= msg_tag 		;
-    in_data.range(223,192) 	= buf0_type 	;
-    in_data.range(255,224) 	= buf1_type 	;
-    in_data.range(287,256) 	= buf2_type 	;
-    in_data.range(351,288) 	= addra 	  	;
-    in_data.range(415,352) 	= addrb    		;
-    in_data.range(479,416) 	= addrc 		;
-    {
-		#pragma HLS protocol fixed 
-		cmd.write(in_data);
-		ap_wait();
-		sts.read();
-	}
+void dummy_hls_control(
+    ap_uint<32> scenario,
+    ap_uint<32> len,
+    ap_uint<32> comm,
+    ap_uint<32> root_src_dst,
+    ap_uint<32> function,
+    ap_uint<32> msg_tag,
+    ap_uint<32> buf0_type,
+    ap_uint<32> buf1_type,
+    ap_uint<32> buf2_type,
+    ap_uint<64> addra,
+    ap_uint<64> addrb,
+    ap_uint<64> addrc,
+    ap_uint<64> delay,
+    stream<ap_uint<512>> &cmd,
+    stream<ap_uint<32>> &sts) {
+#pragma HLS INTERFACE s_axilite port = scenario
+#pragma HLS INTERFACE s_axilite port = len
+#pragma HLS INTERFACE s_axilite port = comm
+#pragma HLS INTERFACE s_axilite port = root_src_dst
+#pragma HLS INTERFACE s_axilite port = function
+#pragma HLS INTERFACE s_axilite port = msg_tag
+#pragma HLS INTERFACE s_axilite port = buf0_type
+#pragma HLS INTERFACE s_axilite port = buf1_type
+#pragma HLS INTERFACE s_axilite port = buf2_type
+#pragma HLS INTERFACE s_axilite port = addra
+#pragma HLS INTERFACE s_axilite port = addrb
+#pragma HLS INTERFACE s_axilite port = addrc
+#pragma HLS INTERFACE s_axilite port = delay
+#pragma HLS INTERFACE axis port = cmd
+#pragma HLS INTERFACE axis port = sts
+#pragma HLS INTERFACE s_axilite port = return
+
+  ap_wait_n(delay);
+
+  ap_uint<512> in_data;
+  // Input stream needs to be optimized in the same way as hostctrl
+  in_data.range(31, 0) = scenario;
+  in_data.range(63, 32) = len;
+  in_data.range(95, 64) = comm;
+  in_data.range(127, 96) = root_src_dst;
+  in_data.range(159, 128) = function;
+  in_data.range(191, 160) = msg_tag;
+  in_data.range(223, 192) = buf0_type;
+  in_data.range(255, 224) = buf1_type;
+  in_data.range(287, 256) = buf2_type;
+  in_data.range(351, 288) = addra;
+  in_data.range(415, 352) = addrb;
+  in_data.range(479, 416) = addrc;
+  {
+#pragma HLS protocol fixed
+    cmd.write(in_data);
+    ap_wait();
+    sts.read();
+  }
 }
--- demo/build/hls/dummy_hls_control/tb_dummy_hls_control.cpp
+++ demo/build/hls/dummy_hls_control/tb_dummy_hls_control.cpp
@@ -13,74 +13,78 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-# *******************************************************************************/
+#
+*******************************************************************************/
 
-#include "hls_stream.h"
+#include <iostream>
+
 #include "ap_int.h"
-#include<iostream>
+#include "hls_stream.h"
 
 using namespace hls;
 using namespace std;
 
-void dummy_hls_control(	ap_uint<32> scenario,
-				ap_uint<32> len,
-				ap_uint<32> comm,
-				ap_uint<32> root_src_dst,
-				ap_uint<32> function,
-				ap_uint<32> msg_tag,
-				ap_uint<32> buf0_type,
-				ap_uint<32> buf1_type,
-				ap_uint<32> buf2_type,
-				ap_uint<64> addra,
-				ap_uint<64> addrb,
-				ap_uint<64> addrc,
-				stream<ap_uint<512>> &cmd,
-				stream<ap_uint<32>> &sts
-);
+void dummy_hls_control(
+    ap_uint<32> scenario,
+    ap_uint<32> len,
+    ap_uint<32> comm,
+    ap_uint<32> root_src_dst,
+    ap_uint<32> function,
+    ap_uint<32> msg_tag,
+    ap_uint<32> buf0_type,
+    ap_uint<32> buf1_type,
+    ap_uint<32> buf2_type,
+    ap_uint<64> addra,
+    ap_uint<64> addrb,
+    ap_uint<64> addrc,
+    ap_uint<64> delay,
+    stream<ap_uint<512>> &cmd,
+    stream<ap_uint<32>> &sts);
 
-int main(){
-	int nerrors = 0;
+int main() {
+  int nerrors = 0;
 
-	stream<ap_uint<512>> cmd;
-	stream<ap_uint<32>> sts;
-	
-	for(int i = 0; i < 10; i++){
+  stream<ap_uint<512>> cmd;
+  stream<ap_uint<32>> sts;
 
-		ap_uint<32> scenario 	= 1 + i; 
-		ap_uint<32> len 		= 2 + i; 
-		ap_uint<32> comm		= 3 + i; 
-		ap_uint<32> root_src_dst= 4 + i; 
-		ap_uint<32> function 	= 5 + i; 
-		ap_uint<32> msg_tag 	= 6 + i; 
-		ap_uint<32> buf0_type 	= 7 + i; 
-		ap_uint<32> buf1_type 	= 8 + i; 
-		ap_uint<32> buf2_type 	= 9 + i; 
-		ap_uint<64> addra 	  	= 10 + i; 
-		ap_uint<64> addrb    	= 11 + i; 
-		ap_uint<64> addrc 		= 12 + i; 
-		sts.write( 1 + i );
-		dummy_hls_control(scenario, len, comm, root_src_dst, function, msg_tag, buf0_type, buf1_type, buf2_type, addra, addrb, addrc, cmd, sts);
-		ap_uint<512> cmd_tmp = cmd.read();
-		nerrors += (     scenario 	  != cmd_tmp.range(31,0)	);
-		nerrors += (     len 		  != cmd_tmp.range(63,32)	);
-		nerrors += (     comm		  != cmd_tmp.range(95,64)	);
-		nerrors += (     root_src_dst != cmd_tmp.range(127,96)	);
-		nerrors += (     function 	  != cmd_tmp.range(159,128) );
-		nerrors += (     msg_tag 	  != cmd_tmp.range(191,160) );
-		nerrors += (     buf0_type 	  != cmd_tmp.range(223,192) );
-		nerrors += (     buf1_type 	  != cmd_tmp.range(255,224) );
-		nerrors += (     buf2_type 	  != cmd_tmp.range(287,256) );
-		nerrors += (     addra 	  	  != cmd_tmp.range(351,288) );
-		nerrors += (     addrb    	  != cmd_tmp.range(415,352) );
-		nerrors += (     addrc 		  != cmd_tmp.range(479,416) );
-	}
+  for (int i = 0; i < 10; i++) {
+    ap_uint<32> scenario = 1 + i;
+    ap_uint<32> len = 2 + i;
+    ap_uint<32> comm = 3 + i;
+    ap_uint<32> root_src_dst = 4 + i;
+    ap_uint<32> function = 5 + i;
+    ap_uint<32> msg_tag = 6 + i;
+    ap_uint<32> buf0_type = 7 + i;
+    ap_uint<32> buf1_type = 8 + i;
+    ap_uint<32> buf2_type = 9 + i;
+    ap_uint<64> addra = 10 + i;
+    ap_uint<64> addrb = 11 + i;
+    ap_uint<64> addrc = 12 + i;
+    sts.write(1 + i);
+    dummy_hls_control(
+        scenario, len, comm, root_src_dst, function, msg_tag, buf0_type,
+        buf1_type, buf2_type, addra, addrb, addrc, i, cmd, sts);
+    ap_uint<512> cmd_tmp = cmd.read();
+    nerrors += (scenario != cmd_tmp.range(31, 0));
+    nerrors += (len != cmd_tmp.range(63, 32));
+    nerrors += (comm != cmd_tmp.range(95, 64));
+    nerrors += (root_src_dst != cmd_tmp.range(127, 96));
+    nerrors += (function != cmd_tmp.range(159, 128));
+    nerrors += (msg_tag != cmd_tmp.range(191, 160));
+    nerrors += (buf0_type != cmd_tmp.range(223, 192));
+    nerrors += (buf1_type != cmd_tmp.range(255, 224));
+    nerrors += (buf2_type != cmd_tmp.range(287, 256));
+    nerrors += (addra != cmd_tmp.range(351, 288));
+    nerrors += (addrb != cmd_tmp.range(415, 352));
+    nerrors += (addrc != cmd_tmp.range(479, 416));
+  }
 
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << "******************************"<< std::endl;
-	std::cout << "Found " << nerrors << " errors"<< std::endl;
-	std::cout << "******************************"<< std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	return 0;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << "******************************" << std::endl;
+  std::cout << "Found " << nerrors << " errors" << std::endl;
+  std::cout << "******************************" << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  return 0;
 }
