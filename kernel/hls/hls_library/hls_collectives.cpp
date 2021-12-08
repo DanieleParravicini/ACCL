123e2a7513ec8fb40caa2d3f1bc4276da65376c4
Alberto Zeni <alberto.zeni@polimi.it>
2021-12-08 15:15:45 +0100

    readded interaction with cclo within each collective call

--- kernel/hls/hls_library/hls_collectives.cpp
+++ kernel/hls/hls_library/hls_collectives.cpp
@@ -19,19 +19,26 @@
 #include "ap_int.h"
 #include "ap_utils.h"
 #include "hls_collectives.h"
+#include "hostctrl_in.h"
 
 using namespace hls;
 using namespace std;
 
 #define DATA_WIDTH 512
 
-ap_uint<DATA_WIDTH> send_in(
+ap_uint<32> send_in(
     unsigned int comm,
     unsigned int len,
     unsigned int tag,
     unsigned int dst_rank,
     uint64_t buf_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return 
+
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_SEND;
     in_data.range(END_LEN,START_LEN) = len;
@@ -40,16 +47,28 @@ ap_uint<DATA_WIDTH> send_in(
     in_data.range(END_TAG,START_TAG) = tag;
     in_data.range(END_ADDR_A,START_ADDR_A) = buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> recv_in(
+ap_uint<32> recv_in(
     unsigned int comm,
     unsigned int len,
     unsigned int tag,
     unsigned int src_rank,
     uint64_t buf_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return 
+    
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_RECV;
     in_data.range(END_LEN,START_LEN) = len;
@@ -58,15 +77,26 @@ ap_uint<DATA_WIDTH> recv_in(
     in_data.range(END_TAG,START_TAG) = tag;
     in_data.range(END_ADDR_A,START_ADDR_A) = buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> broadcast_in(
+ap_uint<32> broadcast_in(
     unsigned int comm,
     unsigned int len,
     unsigned int src_rank,
     uint64_t buf_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_BCAST;
     in_data.range(END_LEN,START_LEN) = len;
@@ -74,10 +104,16 @@ ap_uint<DATA_WIDTH> broadcast_in(
     in_data.range(END_ROOT_SRC_DST,START_ROOT_SRC_DST) = src_rank;
     in_data.range(END_ADDR_A,START_ADDR_A) = buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> scatter_in(
+ap_uint<32> scatter_in(
     unsigned int comm,
     unsigned int len,
     unsigned int src_rank,
@@ -85,6 +121,11 @@ ap_uint<DATA_WIDTH> scatter_in(
     uint64_t dst_buf_addr
 )
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_SCATTER;
     in_data.range(END_LEN,START_LEN) = len;
@@ -93,16 +134,27 @@ ap_uint<DATA_WIDTH> scatter_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_buf_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> gather_in(
+ap_uint<32> gather_in(
     unsigned int comm,
     unsigned int len,
     unsigned int root_rank,
     uint64_t src_buf_addr,
     uint64_t dst_buf_addr
 ){
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_GATHER;
     in_data.range(END_LEN,START_LEN) = len;
@@ -111,15 +163,26 @@ ap_uint<DATA_WIDTH> gather_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_buf_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> allgather_in(
+ap_uint<32> allgather_in(
     unsigned int comm,
     unsigned int len,
     uint64_t src_buf_addr,
     uint64_t dst_buf_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_ALLGATHER;
     in_data.range(END_LEN,START_LEN) = len;
@@ -127,10 +190,16 @@ ap_uint<DATA_WIDTH> allgather_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_buf_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_buf_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> reduce_in(
+ap_uint<32> reduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
@@ -138,6 +207,11 @@ ap_uint<DATA_WIDTH> reduce_in(
     uint64_t src_addr,
     uint64_t dst_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_REDUCE;
     in_data.range(END_LEN,START_LEN) = len;
@@ -147,16 +221,27 @@ ap_uint<DATA_WIDTH> reduce_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> allreduce_in(
+ap_uint<32> allreduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
     uint64_t src_addr,
     uint64_t dst_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_ALLREDUCE;
     in_data.range(END_LEN,START_LEN) = len;
@@ -165,24 +250,46 @@ ap_uint<DATA_WIDTH> allreduce_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> config_in(unsigned int function)
+ap_uint<32> config_in(unsigned int function)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_CONFIG;
     in_data.range(END_FUNCTION,START_FUNCTION) = function;
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> accumulate_in(
+ap_uint<32> accumulate_in(
     unsigned int len,
     unsigned int function,
     uint64_t op0_addr,
     uint64_t op1_addr)//OP1_ADDR = DST_ADDR
     // uint64_t dst_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_ACC;
     in_data.range(END_LEN,START_LEN) = len;
@@ -190,43 +297,76 @@ ap_uint<DATA_WIDTH> accumulate_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = op0_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = op1_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> copy_in(
+ap_uint<32> copy_in(
     unsigned int len,
     uint64_t src_addr,
     uint64_t dst_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_COPY;
     in_data.range(END_LEN,START_LEN) = len;
     in_data.range(END_ADDR_A,START_ADDR_A) = src_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> ext_kernel_stream_in(
+ap_uint<32> ext_kernel_stream_in(
     unsigned int len,
     uint64_t src_addr,
     uint64_t dst_addr)
 {
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_EXT_STREAM_KRNL;
     in_data.range(END_LEN,START_LEN) = len;
     in_data.range(END_ADDR_A,START_ADDR_A) = src_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> reduce_ext_in(
+ap_uint<32> reduce_ext_in(
     unsigned int len,
     uint64_t op1_addr,
     uint64_t op2_addr,
     uint64_t dst_addr)
-{    
+{
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return    
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_EXT_REDUCE;
     in_data.range(END_LEN,START_LEN) = len;
@@ -234,16 +374,27 @@ ap_uint<DATA_WIDTH> reduce_ext_in(
     in_data.range(END_ADDR_B,START_ADDR_B) = op2_addr;
     in_data.range(END_ADDR_C,START_ADDR_C) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
 
-ap_uint<DATA_WIDTH> scatter_reduce_in(
+ap_uint<32> scatter_reduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
     uint64_t src_addr,
     uint64_t dst_addr)
-{    
+{
+    #pragma HLS INTERFACE axis port=cmd_in
+    #pragma HLS INTERFACE axis port=cmd_out
+    #pragma HLS INTERFACE axis port=sts_in
+    #pragma HLS INTERFACE axis port=sts_out
+    #pragma HLS INTERFACE ap_ctrl_none  port=return    
     ap_uint<DATA_WIDTH> in_data = 0;
     in_data.range(END_SCENARIO,START_SCENARIO)  = ACCL_REDUCE_SCATTER;
     in_data.range(END_LEN,START_LEN) = len;
@@ -252,5 +403,11 @@ ap_uint<DATA_WIDTH> scatter_reduce_in(
     in_data.range(END_ADDR_A,START_ADDR_A) = src_addr;
     in_data.range(END_ADDR_B,START_ADDR_B) = dst_addr;
 
-    return in_data;
+    stream <ap_uint<DATA_WIDTH> > cmd_in;
+    cmd_in.write(in_data);
+    stream <ap_uint<32> > cmd_out;
+    stream <ap_uint<32> > sts_in;
+    stream <ap_uint<32> > sts_out;
+    hostctrl_in(cmd_in,cmd_out,sts_in,sts_out);
+    return sts_out.read();
 }
--- kernel/hls/hls_library/hls_collectives.h
+++ kernel/hls/hls_library/hls_collectives.h
@@ -39,73 +39,73 @@
 //******************************
 //**  PACKET INDEXES          **
 //******************************
-#define START_SCENARIO 0
-#define END_SCENARIO (START_SCENARIO+32-1)
-#define START_LEN (END_SCENARIO+1)
-#define END_LEN (START_LEN+32-1)
-#define START_COMM (END_LEN+1)
-#define END_COMM (START_COMM+32-1)
-#define START_ROOT_SRC_DST (END_COMM+1)
-#define END_ROOT_SRC_DST (START_ROOT_SRC_DST+32-1)
-#define START_FUNCTION (END_ROOT_SRC_DST+1)
-#define END_FUNCTION (START_FUNCTION+32-1)
-#define START_TAG (END_FUNCTION+1)
-#define END_TAG (START_TAG+32-1)
-#define START_BUFF_0_TYPE (END_TAG+1)
-#define END_BUFF_0_TYPE (START_BUFF_0_TYPE+32-1)
-#define START_BUFF_1_TYPE (END_BUFF_0_TYPE+1)
-#define END_BUFF_1_TYPE (START_BUFF_1_TYPE+32-1)
-#define START_BUFF_2_TYPE (END_BUFF_1_TYPE+1)
-#define END_BUFF_2_TYPE (START_BUFF_2_TYPE+32-1)
-#define START_ADDR_A (END_BUFF_2_TYPE+1)
-#define END_ADDR_A (START_ADDR_A+64-1)
-#define START_ADDR_B (END_ADDR_A+1)
-#define END_ADDR_B (START_ADDR_B+64-1)
-#define START_ADDR_C (END_ADDR_B+1)
-#define END_ADDR_C (START_ADDR_C+64-1)
+// #define START_SCENARIO 0
+// #define END_SCENARIO (START_SCENARIO+32-1)
+// #define START_LEN (END_SCENARIO+1)
+// #define END_LEN (START_LEN+32-1)
+// #define START_COMM (END_LEN+1)
+// #define END_COMM (START_COMM+32-1)
+// #define START_ROOT_SRC_DST (END_COMM+1)
+// #define END_ROOT_SRC_DST (START_ROOT_SRC_DST+32-1)
+// #define START_FUNCTION (END_ROOT_SRC_DST+1)
+// #define END_FUNCTION (START_FUNCTION+32-1)
+// #define START_TAG (END_FUNCTION+1)
+// #define END_TAG (START_TAG+32-1)
+// #define START_BUFF_0_TYPE (END_TAG+1)
+// #define END_BUFF_0_TYPE (START_BUFF_0_TYPE+32-1)
+// #define START_BUFF_1_TYPE (END_BUFF_0_TYPE+1)
+// #define END_BUFF_1_TYPE (START_BUFF_1_TYPE+32-1)
+// #define START_BUFF_2_TYPE (END_BUFF_1_TYPE+1)
+// #define END_BUFF_2_TYPE (START_BUFF_2_TYPE+32-1)
+// #define START_ADDR_A (END_BUFF_2_TYPE+1)
+// #define END_ADDR_A (START_ADDR_A+64-1)
+// #define START_ADDR_B (END_ADDR_A+1)
+// #define END_ADDR_B (START_ADDR_B+64-1)
+// #define START_ADDR_C (END_ADDR_B+1)
+// #define END_ADDR_C (START_ADDR_C+64-1)
 
 
-ap_uint<DATA_WIDTH> send_in(
+ap_uint<32> send_in(
     unsigned int comm,
     unsigned int len,
     unsigned int tag,
     unsigned int dst_rank,
     uint64_t buf_addr);
 
-ap_uint<DATA_WIDTH> recv_in(
+ap_uint<32> recv_in(
     unsigned int comm,
     unsigned int len,
     unsigned int tag,
     unsigned int src_rank,
     uint64_t buf_addr);
 
-ap_uint<DATA_WIDTH> broadcast_in(
+ap_uint<32> broadcast_in(
     unsigned int comm,
     unsigned int len,
     unsigned int src_rank,
     uint64_t buf_addr);
 
-ap_uint<DATA_WIDTH> scatter_in(
+ap_uint<32> scatter_in(
     unsigned int comm,
     unsigned int len,
     unsigned int src_rank,
     uint64_t src_buf_addr,
     uint64_t dst_buf_addr);
 
-ap_uint<DATA_WIDTH> gather_in(
+ap_uint<32> gather_in(
     unsigned int comm,
     unsigned int len,
     unsigned int root_rank,
     uint64_t src_buf_addr,
     uint64_t dst_buf_addr);
 
-ap_uint<DATA_WIDTH> allgather_in(
+ap_uint<32> allgather_in(
     unsigned int comm,
     unsigned int len,
     uint64_t src_buf_addr,
     uint64_t dst_buf_addr);
 
-ap_uint<DATA_WIDTH> reduce_in(
+ap_uint<32> reduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
@@ -113,38 +113,38 @@ ap_uint<DATA_WIDTH> reduce_in(
     uint64_t src_addr,
     uint64_t dst_addr);
 
-ap_uint<DATA_WIDTH> allreduce_in(
+ap_uint<32> allreduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
     uint64_t src_addr,
     uint64_t dst_addr);
 
-ap_uint<DATA_WIDTH> config_in(unsigned int function);
+ap_uint<32> config_in(unsigned int function);
 
-ap_uint<DATA_WIDTH> accumulate_in(
+ap_uint<32> accumulate_in(
     unsigned int len,
     unsigned int function,
     uint64_t op0_addr,
     uint64_t op1_addr);
 
-ap_uint<DATA_WIDTH> copy_in(
+ap_uint<32> copy_in(
     unsigned int len,
     uint64_t src_addr,
     uint64_t dst_addr);
 
-ap_uint<DATA_WIDTH> ext_kernel_stream_in(
+ap_uint<32> ext_kernel_stream_in(
     unsigned int len,
     uint64_t src_addr,
     uint64_t dst_addr);
 
-ap_uint<DATA_WIDTH> reduce_ext_in(
+ap_uint<32> reduce_ext_in(
     unsigned int len,
     uint64_t op1_addr,
     uint64_t op2_addr,
     uint64_t dst_addr);
 
-ap_uint<DATA_WIDTH> scatter_reduce_in(
+ap_uint<32> scatter_reduce_in(
     unsigned int comm,
     unsigned int len,
     unsigned int function,
